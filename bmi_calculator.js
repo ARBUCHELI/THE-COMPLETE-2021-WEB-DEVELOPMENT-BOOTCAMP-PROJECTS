99fe055401a3d437d4f40ef14c186200962a8974
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-04-12 11:52:13 +0300

    Update bmi_calculator.js

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -1,7 +1,18 @@
 //Create your function below this line.
 //The first parameter should be the weight and the second should be the height.
 function bmiCalculator (weight, height) {
-    return Math.round(weight /(Math.pow(height,2)));
+    var message = "";
+    var bmi = Math.round(weight /(Math.pow(height,2)));
+    if (bmi < 18.5) {
+        message = "Your BMI is "+bmi+", so you are underweight.";
+    }
+    if (bmi >= 18.5 && bmi <24.9) {
+        message = "Your BMI is "+bmi+", so you have a normal weight.";
+    }
+    if (bmi > 24.9) {
+        message = "Your BMI is "+bmi+", so you are overweight.";
+    }
+    return message;
 }
 
 
@@ -13,5 +24,5 @@ var bmi = bmiCalculator(65, 1.8);
 bmi should equal 20 when it's rounded to the nearest whole number.
 
 */
-var bmi = bmiCalculator(65, 1.8);
-console.log(bmi);
+var answer = bmiCalculator(65, 1.8);
+console.log(answer);
