434155b3f60e7167b753bafc70034019329b1d42
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-04-12 20:10:40 +0300

    Update fizzfuzz.js

--- fizzfuzz.js
+++ fizzfuzz.js
@@ -1,18 +1,22 @@
 var output = [];
 var count = 1; // --> The biggest mistake here was to put the variable inside of the function as a local variable and not as a global.
 function fizzfuzz() {
-    if ((count % 3 === 0) && (count % 5 === 0)) {
-        output.push("fizzfuzz");
+    while (count <= 100) {
+        if ((count % 3 === 0) && (count % 5 === 0)) {
+            output.push("fizzfuzz");
+        }
+        else if (count % 3 === 0) {
+            output.push("fizz");
+        }
+        else if (count % 5 === 0) {
+            output.push("fuzz");
+        }
+        else {
+            output.push(count);
+        }
+        count++;
+        console.log(output);
     }
-    else if (count % 3 === 0) {
-        output.push("fizz");
-    }
-    else if (count % 5 === 0) {
-        output.push("fuzz");
-    }
-    else {
-        output.push(count);
-    }
-    count++;
-    console.log(output);
 }
+
+fizzfuzz()
