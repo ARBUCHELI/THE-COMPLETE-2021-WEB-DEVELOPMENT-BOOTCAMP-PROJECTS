1f6cac8bf857a78b71bd735cb26c81c39eb3d932
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-05-24 12:16:35 +0300

    Add files via upload

---
+++ Real Time Clock App/src/components/App.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+
+function App() {
+  setInterval(getTime, 1000);
+  const now = new Date().toLocaleTimeString()
+ 
+  const [time, setTime] = useState(now);
+  
+  function getTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
+ 
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={getTime}>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
