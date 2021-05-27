33cfc6dc0e1ef6e0b1be8e7bb9b4f569c693bcfb
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-05-27 19:20:41 +0300

    Add files via upload

---
+++ Managing a Component Tree/src/components/TodoItem.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+function TodoItem (props) {
+  return (
+    <div 
+      onClick={() => {
+        props.onChecked(props.id);
+      }}>
+      <li>
+        {props.text} 
+      </li>
+    </div>
+  )
+}
+
+export default TodoItem
